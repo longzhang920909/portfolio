1ee25d6683853852f94528d5cf40f3eda0490190
Emilia <81705094+isemilia@users.noreply.github.com>
2024-03-29 22:06:41 +0500

    Reduce dot opacity

--- src/components/banner/resources/styles.ts
+++ src/components/banner/resources/styles.ts
@@ -10,7 +10,7 @@ export const StyledDot = styled('div')(() => ({
   position: 'absolute',
   pointerEvents: 'none',
   span: {
-    opacity: .8,
+    opacity: .6,
     display: 'block',
     width: '100%',
     height: '100%',
