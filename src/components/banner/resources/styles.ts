1fe5618ffd2c0a4011c4357fa1c1d994bad9768b
Emilia <81705094+isemilia@users.noreply.github.com>
2024-04-07 11:01:20 +0500

    Refactor banner styles

--- src/components/banner/resources/styles.ts
+++ src/components/banner/resources/styles.ts
@@ -3,7 +3,17 @@ import styled from "styled-components";
 export const StyledBannerWrap = styled('div')(({theme}) => ({
   background: `linear-gradient(180deg, ${theme.palette.primary.extraDark}, transparent)`,
   height: '200px',
-  position: 'relative'
+  position: 'relative',
+  '& .particle, .glow': {
+    position: 'absolute',
+    pointerEvents: 'none'
+  },
+  '& .glow': {
+    width: '1px',
+    height: '1px',
+    background: 'rgba(255, 255, 255, 0)',
+    boxShadow: '0 0 20px 10px rgba(222, 91, 255, 0.1)',
+  }
 }));
 
 export const StyledDot = styled('div')(() => ({
