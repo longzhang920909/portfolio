923cebcc4ea71ceee26444ad6fcc65a9c0e8b2d9
Emilia <81705094+isemilia@users.noreply.github.com>
2024-04-14 20:57:52 +0500

    Create base icon-link

---
+++ src/ui/icon-link/assets/icon-link.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+// import Text from '../../text';
+
+export const StyledIconLink = styled('a')(() => ({
+  display: 'inline-flex',
+}));
---
+++ src/ui/icon-link/icon-link.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { StyledIconLink } from './assets/icon-link.styles.ts';
+
+const IconLink: FC = () => {
+  return <StyledIconLink></StyledIconLink>;
+};
+
+export default IconLink;
